7f1f1574201a7fb3da1b5ee53c383d2ed72e3e51
Alex Ulfich <dev-aulfich@gmail.com>
2020-08-27 18:35:41 -0600

    issue-6: added travel form wireframe and cdns

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,1 +1,21 @@
 console.log(`this is linked!`);
+
+/* CREATE PAGE */
+// create form fields
+const $startDateInput = $(`#startDate`);
+const $endDateInput = $(`#endDate`);
+const $destinationInput = $(`#destination`);
+// const $campingInput = $(`#camping`);
+// const $businessInput = $(`#business`);
+// const $roadTripInput = $(`#roadTrip`);
+// create form button
+const $createBtn = $(`#createBtn`);
+
+$createBtn.click(function(e) {
+    e.preventDefault();
+    
+    const startDateValue = $startDateInput.val();
+    const endDateValue = $endDateInput.val();
+    const destinationValue = $destinationInput.val();
+    const typeValue = $('.form__radio:checked');
+});
