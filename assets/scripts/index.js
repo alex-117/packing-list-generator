a0cecc07ad6b5c85ff0ac3fb94d84595a46dc6ff
Alex Ulfich <alexulfich@gmail.com>
2020-08-29 15:05:56 -0600

    issue-10: Getting locationDetails & using res to get locationWeather

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,19 +1,66 @@
-/* CREATE PAGE */
+/** Using `cors-anywhere` proxy to solve CORS error */
+const url = 'https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api';
+
 // create form fields
 const $startDateInput = $(`#startDate`);
 const $endDateInput = $(`#endDate`);
 const $destinationInput = $(`#destination`);
-// const $campingInput = $(`#camping`);
-// const $businessInput = $(`#business`);
-// const $roadTripInput = $(`#roadTrip`);
 // create form button
 const $createBtn = $(`#createBtn`);
 
-$createBtn.click(function(e) {
-    e.preventDefault();
-    
-    const startDateValue = $startDateInput.val();
-    const endDateValue = $endDateInput.val();
-    const destinationValue = $destinationInput.val();
-    const typeValue = $('.form__radio:checked');
+
+$createBtn.click(async (e) => {
+  e.preventDefault();
+
+  const startDateValue = $startDateInput.val();
+  const endDateValue = $endDateInput.val();
+  const destinationValue = $destinationInput.val();
+  const typeValue = $('.form__radio:checked');
+
+  const queryUrl = `${url}/location/search/?query=${destinationValue}`;
+
+  const locationDetails = await getLocationDetails(queryUrl);
+  // if startDate > 5 days in future
+  // getLocationByDay (need to set up still, /api/location/(woeid)/(date)/
+
+  // else
+  const locationById = await getLocationById(locationDetails.woeid);
 });
+
+/**
+ * @param queryUrl
+ * @returns {Promise<location>}
+ */
+const getLocationDetails = (queryUrl) => {
+  return $.ajax(queryUrl)
+    .then((response) => {
+      // if no match is found, returns empty []
+      if (!response.length) {
+        throw Error('Empty response');
+      }
+
+      return response[0]; // get first response and hope it's the right one :)
+    })
+    .catch((error) => {
+      // TODO: handle error messages
+      console.log(error);
+    });
+};
+
+/**
+ * @param woeid
+ * @returns {Promise<T>}
+ */
+const getLocationById = (woeid) => {
+  return $.ajax(`${url}/location/${woeid}`)
+    .then((response) => {
+      if (!response) {
+        throw Error('Invalid ID');
+      }
+      return response;
+    })
+    .catch((error) => {
+      // TODO: handle error messages
+      console.log(error);
+    });
+};
