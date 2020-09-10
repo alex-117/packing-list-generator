6aa52713f76322471bc84b283afce37875acf426
Alex Ulfich <dev-aulfich@gmail.com>
2020-09-09 21:39:06 -0600

    issue-17: added logic for edit/view.html

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -44,7 +44,6 @@ function handleLocationDetails(location) {
         // console.log(locationDetails)
       });
   }
-
 }
 
 function handleHistoricalData(data) {
@@ -88,13 +87,13 @@ function handleHistoricalData(data) {
     weather.temp.max = convertToFahrenheit(weather.temp.max);
     weather.temp.min = convertToFahrenheit(weather.temp.min);
 
-    generatePackingListUI(weather);
+    generateNewPackingList(weather);
   } catch (e) {
     console.log(e)
   }
 }
 
-function generatePackingListUI(weather) {
+function generateNewPackingList(weather) {
   // take packing list & weather conditions
   // getTravelTypePackingList($typeInput.val())
   getTravelTypePackingList('camping-no-rv')
@@ -104,8 +103,22 @@ function generatePackingListUI(weather) {
       if (!weather.conditions.snow) delete list.snow;
       if (!weather.conditions.sunny) delete list.sunny;
 
-      for (let key in list) {
-        packingListDisplay(key, list[key]);
+      for (let category in list) {
+        const packingItems = list[category];
+
+        const $listHeader = $(`<h3 class="form_packing-list-header">`);
+        $listHeader.text(category);
+
+        const $listWrapper = $(`<ul class="form__packing-list">`);
+
+        $packingLists.append($listHeader);
+        $packingLists.append($listWrapper);
+
+        for (let packingItem of packingItems) {
+          const newListItem = packingListItem(packingItem, category);
+          $listWrapper.append(newListItem);
+        }
+
       }
 
     })
@@ -136,6 +149,27 @@ function generateListForLocalStorage() {
   return packingList;
 }
 
+function packingListItem(item, category) {
+  const $listItem = $(`<li class="form__packing-list-item">`);
+  const $itemLabel = $(`<label class="form__list-item-label" />`);
+  const $itemCheckbox = $(`<input class="form__list-item-checkbox" />`);
+
+  $itemLabel.attr(`for`, `${category}-${item.name}`);
+  $itemLabel.text(item.name);
+
+  $itemCheckbox.data(`type`, `${category}`);
+  $itemCheckbox.attr(`id`, `${category}-${item.name}`);
+  $itemCheckbox.attr(`name`, `packing-item`);
+  $itemCheckbox.attr(`type`, `checkbox`);
+  $itemCheckbox.prop(`checked`, item.checked);
+  $itemCheckbox.val(item.name);
+
+
+  $listItem.append($itemCheckbox);
+  $listItem.append($itemLabel);
+  return $listItem;
+}
+
 function packingListDisplay(headerText, listItems) {
   try {
 
@@ -148,29 +182,37 @@ function packingListDisplay(headerText, listItems) {
     $packingLists.append($listWrapper);
 
     for (let i = 0; i < listItems.length; i++) {
-      const item = listItems[i];
-      const $listItem = $(`<li class="form__packing-list-item">`);
-      const $itemLabel = $(`<label class="form__list-item-label" />`);
-      const $itemCheckbox = $(`<input class="form__list-item-checkbox" />`);
-
-      $itemLabel.attr(`for`, `${headerText}-${i}`);
-      $itemLabel.text(item);
-
-      $itemCheckbox.data(`type`, `${headerText}`);
-      $itemCheckbox.attr(`id`, `${headerText}-${i}`);
-      $itemCheckbox.attr(`name`, `packing-item`);
-      $itemCheckbox.attr(`type`, `checkbox`);
-      $itemCheckbox.val(item);
-
-
-      $listItem.append($itemCheckbox);
-      $listItem.append($itemLabel);
-      $listWrapper.append($listItem);
-
+      const packingItem = packingListItem(listItems[i]);
+      $listWrapper.append(packingItem);
     }
   } catch (e) {
     console.log(e)
   }
+}
+
+function editPackingListDisplay(list) {
+  try {
+      const $editPackingListName = $(`#edit-packing-list-name`);
+      $editPackingListName.val(list.name);
+
+      for (let category in list.items) {
+        const packingItems = list.items[category];
+
+        const $listHeader = $(`<h3 class="form_packing-list-header">`);
+        $listHeader.text(category);
+
+        const $listWrapper = $(`<ul class="form__packing-list">`);
+
+        $packingLists.append($listHeader);
+        $packingLists.append($listWrapper);
+
+        for (let packingItem of packingItems) {
+          const newListItem = packingListItem(packingItem, category);
+          $listWrapper.append(newListItem);
+        }
+
+      }
+  } catch (e) { console.log(e) }
 }
 
 $(document).ready(() => {
@@ -197,7 +239,7 @@ $(document).ready(() => {
     e.preventDefault();
 
     const packingList = generateListForLocalStorage();
-    const listName = prompt(`What would you like to name your new travel list?`);
+    const listName = $(`#packing-list-name`).val();
     savePackingListToStorage(listName, packingList)
   });
 
@@ -205,5 +247,48 @@ $(document).ready(() => {
     // prevent end date from occuring BEFORE start date
     $endDateInput.attr({ min: $startDateInput.val() });
   });
+
+
+  if (window.location.pathname === `/view.html`) {
+    const storedLists = getPackingListFromStorage();
+
+    for (let list of storedLists) {
+      const $listContainer = $(`<div class="stored-lists__list">`);
+
+      const $listName = $(`<h2 class="stored-lists__list-name">`);
+      $listName.text(list.name);
+
+      const $listQuantityPacked = $(`<span class="stored-lists__list-quantity">`);
+      $listQuantityPacked.text(`x / y items packed`);
+
+      const $listEditBtn = $(`<a class="stored-lists__edit-btn">`);
+      $listEditBtn.text(`edit`);
+      $listEditBtn.attr(`href`, `/edit.html?listname=${encodeURIComponent(list.name)}`);
+
+
+      $listContainer.append($listName);
+      $listContainer.append($listQuantityPacked);
+      $listContainer.append($listEditBtn);
+
+      $storedListDisplay.append($listContainer);
+      console.log('hit');
+    }
+  }
+
+  if (window.location.pathname === `/edit.html`) {
+    const storedLists = getPackingListFromStorage();
+    const urlParams = new URLSearchParams(window.location.search);
+    const listNameParam = urlParams.get('listname');
+
+
+    const listToEdit = storedLists.find(function (list) {
+      return list.name === listNameParam;
+    });
+
+    $updatePackingListBtn.data(`listName`, listToEdit.name);
+
+    editPackingListDisplay(listToEdit);
+    $packingListForm.show();
+  }
 
 });
